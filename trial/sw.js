a6f2375efb5e18a93d6328e01d49fd8ab09772e6
dapoeropal <dapoeropal@gmail.com>
2026-06-17 11:43:41 +0700

    Add files via upload

---
+++ trial/sw.js
@@ -0,0 +1,27 @@
+// UBAH NAMA CACHE MENJADI V2 AGAR SISTEM TAHU INI VERSI BARU
+const CACHE_NAME = "gla-sys-v2"; 
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(["./", "./index.html", "./style.css", "./script.js", "./manifest.json"]);
+        })
+    );
+});
+
+// EVENT ACTIVATE: Menghapus ingatan (Cache) file yang lama
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
